be1adf143db8d71f499399aa89a192b5f0b69593
Saif-Ur-Rehman19 <saifurrrehmanparwak123@gmail.com>
2024-05-01 23:27:00 +0500

    first commit

---
+++ app/src/main/java/com/example/recipeapp/MainViewModel.kt
@@ -0,0 +1,37 @@
+package com.example.recipeapp
+
+import androidx.compose.runtime.State
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.launch
+
+class MainViewModel : ViewModel() {
+    private val _categorieState = mutableStateOf(RecipeState())
+    val categoriesState: State<RecipeState> = _categorieState
+    init {
+        fetchCategories()
+    }
+    private fun fetchCategories() {
+        viewModelScope.launch {
+            try {
+                val response = recipeService.getCategories()
+                _categorieState.value = _categorieState.value.copy(
+                    list = response.categories,
+                    loading = false,
+                    error = null
+                )
+            } catch (exception: Exception){
+                _categorieState.value = _categorieState.value.copy(loading = false,
+                    error = "Error fetching categories ${exception.message}"
+                )
+            }
+        }
+    }
+    data class RecipeState(
+        val loading: Boolean = true,
+        val list: List<Category> = emptyList(),
+        val error: String? = null
+    )
+}
---
+++ app/src/main/java/com/example/recipeapp/RecipeScreen.kt
@@ -0,0 +1,72 @@
+package com.example.recipeapp
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import coil.compose.rememberAsyncImagePainter
+
+@Composable
+fun RecipeScreen(modifier: Modifier = Modifier) {
+    val recipeViewModel : MainViewModel = viewModel()
+    val viewState by recipeViewModel.categoriesState
+    Box(modifier = modifier.fillMaxSize()) {
+        when {
+            viewState.loading -> {
+                CircularProgressIndicator(modifier.align(Alignment.Center))
+            }
+            viewState.error != null -> {
+                Text(text = "Error Occurred")
+            }
+            else -> {
+                //display categories
+                CategoryScreen(categories = viewState.list)
+            }
+        }
+    }
+}
+
+@Composable
+fun CategoryScreen(categories: List<Category>) {
+    LazyVerticalGrid(columns = GridCells.Fixed(2), modifier = Modifier.fillMaxSize()) {
+        items(categories) {
+            category -> CategoryItem(category = category)
+        }
+    }
+}
+
+@Composable
+fun CategoryItem(category: Category) {
+    Column(modifier = Modifier
+        .padding(8.dp)
+        .fillMaxSize()) {
+        Image(painter = rememberAsyncImagePainter(model = category.strCategoryThumb),
+            contentDescription = "", modifier = Modifier
+                .fillMaxSize()
+                .aspectRatio(1f) )
+        Text(
+            text = category.strCategory,
+            color = Color.Black,
+            style = TextStyle(fontWeight = FontWeight.Bold),
+            modifier = Modifier.padding(top = 4.dp)
+        )
+        
+    }
+}
